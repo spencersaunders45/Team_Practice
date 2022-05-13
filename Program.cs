671e16c7d54e751fa219b43e621e60345b8e92c5
eagleye123 <49323274+eagleye123@users.noreply.github.com>
2022-05-13 13:24:59 -0600

    added class and ui

--- Program.cs
+++ Program.cs
@@ -3,8 +3,9 @@
 namespace team_practice{
    class Program{
       static void Main(string[] args){
-         Console.WriteLine("Hello World");
-         Console.WriteLine("Hello World");
+         Ui user = new Ui("Jeff");
+         user.Sayname();
+
       }
    }
 }
